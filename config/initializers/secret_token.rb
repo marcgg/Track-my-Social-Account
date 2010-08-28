91038901528f85645abbf936f5ed2b9209b25868
Marc G Gauthier <marcg.gauthier@gmail.com>
2010-08-28 17:43:45 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tsma::Application.config.secret_token = '327f6bb04b95ecebd47444e98d90dcd4d7b017efb358033b9e437566e89bc5fc30e736e78c98fdf67514f2632659926d05b3942024e0956604dd2ec8ed80fca4'
