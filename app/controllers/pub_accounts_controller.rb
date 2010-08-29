0a433e9e6faf7ddd2484c64f3542085e6bad1698
Marc G Gauthier <marcg.gauthier@gmail.com>
2010-08-29 18:24:30 +0200

    added flash div; added credits before I forget

--- app/controllers/pub_accounts_controller.rb
+++ app/controllers/pub_accounts_controller.rb
@@ -2,7 +2,12 @@ class PubAccountsController < ApplicationController
   def create
     klass = params[:type].constantize
     @pub_account = PubAccount.find_by_account(params[:pub_account][:account])
-    @pub_account = klass.create!(params[:pub_account]) if @pub_account.nil?
+    if @pub_account.nil?
+      @pub_account = klass.create!(params[:pub_account]) 
+      flash[:notice] = "Ok, got it. We'll start tracking this page from now on!"
+    else
+      flash[:notice] = "It looks like someone already asked us to track this page, so here is what we fetched so far!"
+    end
     redirect_to pub_account_path(@pub_account)
   end
   
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,7 +1,12 @@
 $(function(){
-   $(".account").click(function(e){
-      var $this = $(this);
-      $this.find(".form").slideDown("slow");
-      $this.addClass("selected"); 
-   });
+    
+  $(".account").click(function(e){
+    var $this = $(this);
+    $this.find(".form").slideDown("slow");
+    $this.addClass("selected"); 
+  });
+   
+  /* HANDLING NOTICES */
+  $("#flash").delay(1500).slideDown("slow").delay(5000).slideUp("slow");
+  
 });
